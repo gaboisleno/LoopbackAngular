943f2416d35e990a482b10ffd0af3f0a217266be
Gabo <gisleno@codexia.io>
2019-09-18 12:54:02 -0300

    nodemailer

---
+++ scripts/email.js
@@ -0,0 +1,37 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'alvena.goldner73@ethereal.email',
+            pass: '4qVYsGRC2TpHKZ79ud'
+        }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: 'alvena.goldner73@ethereal.email', // sender address
+        to: 'gisleno@codexia.io', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
